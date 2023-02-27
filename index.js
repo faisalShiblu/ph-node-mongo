9381652ac4b48bf5c0b17346f02874bd22f3bf95
Faisal Alam Shiblu <45782662+faisalShiblu@users.noreply.github.com>
2023-02-27 23:49:50 +0600

    mongo db ongoing

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://faisalshiblu0:Shiblu360@cluster0.suhyktt.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 var app = express()
 
 app.get('/', function (req, res) {
-    res.send('hello world')
+    res.send('hello world i am working')
 })
+
+
+
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 
 app.listen(3000, () => console.log('listening to http://localhost/3000'))
