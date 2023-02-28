9cb05bf48ef4d47d6d5477af7a8c007086c9e779
Faisal Alam Shiblu <45782662+faisalShiblu@users.noreply.github.com>
2023-03-01 00:01:36 +0600

    video 4 done

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 var express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 
-const uri = "mongodb+srv://faisalshiblu0:Shiblu360@cluster0.suhyktt.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = "mongodb+srv://faisalshiblu0:Shiblu360@cluster0.suhyktt.mongodb.net/organicdb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 var app = express()
 
 app.get('/', function (req, res) {
-    res.send('hello world i am working')
+    res.sendFile(__dirname + '/index.html');
 })
 
 
-
-
 client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
+    const collection = client.db("organicdb").collection("products");
+    const product = { name: "laptop", price: 1000, quantity: 20 };
+    collection.insertOne(product)
+        .then(result => console.log('product inserted'));
 });
 
 app.listen(3000, () => console.log('listening to http://localhost/3000'))
