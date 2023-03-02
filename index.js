8c9270de2276ec367258ca6733db76982873d215
Faisal Alam Shiblu <45782662+faisalShiblu@users.noreply.github.com>
2023-03-02 21:30:44 +0600

    done

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express')
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 
 const uri = "mongodb+srv://faisalshiblu0:Shiblu360@cluster0.suhyktt.mongodb.net/organicdb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 //const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-var app = express()
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -14,9 +18,42 @@ app.get('/', function (req, res) {
 
 client.connect(err => {
     const collection = client.db("organicdb").collection("products");
-    const product = { name: "laptop", price: 1000, quantity: 20 };
-    collection.insertOne(product)
-        .then(result => console.log('product inserted'));
+
+    app.get("/products", (req, res) => {
+        collection.find({}).toArray((err, documents) => {
+            res.send(documents)
+        })
+    });
+
+    app.post("/addProduct", (req, res) => {
+        const product = req.body;
+        collection.insertOne(product)
+            .then(result => {
+                //console.log('product inserted');
+                // res.send('success');
+                res.redirect('/');
+            });
+    });
+
+    app.get("/product/:id", (req, res) => {
+        collection.find({ _id: ObjectId(req.params.id) }).toArray((err, documents) => {
+            res.send(documents[0]);
+        })
+    });
+
+    app.delete("/delete/:id", (req, res) => {
+        collection.deleteOne({ _id: ObjectId(req.params.id) })
+            .then(result => { res.send(result.deletedCount > 0) })
+    });
+
+    app.patch("/update/:id", (req, res) => {
+        collection.updateOne({ _id: ObjectId(req.params.id) },
+            {
+                $set: { price: req.body.price, quantity: req.body.quantity }
+            })
+            .then(result => { res.send(result.modifiedCount > 0) })
+    })
+
 });
 
 app.listen(3000, () => console.log('listening to http://localhost/3000'))
